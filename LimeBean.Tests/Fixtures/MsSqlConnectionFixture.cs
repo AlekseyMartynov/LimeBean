5bbd9687c66dc86c5b0dbf8b53de079f75b0888c
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2020-01-28 08:37:35 +0300

    SQLEXPRESS → LocalDB

--- LimeBean.Tests/Fixtures/MsSqlConnectionFixture.cs
+++ LimeBean.Tests/Fixtures/MsSqlConnectionFixture.cs
@@ -1,45 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
+using System.IO;
 
 namespace LimeBean.Tests.Fixtures {
 
     public class MsSqlConnectionFixture : ConnectionFixture {
-        ICollection<string> _dropList = new List<string>();
-
-        static string ConnectionString {
-            get { return "server=" + ServerName + "; user instance=true; integrated security=true; connection timeout=90"; }
-        }
-
-        static string ServerName {
-            get { return GetEnvVar("MSSQL_NAME", ".\\SQLEXPRESS"); }
-        }
+        string _dbName;
 
         public MsSqlConnectionFixture() {
-            Connection = new SqlConnection(ConnectionString);
+            Connection = new SqlConnection("server=(localdb)\\MSSQLLocalDB; connection timeout=90");
             Connection.Open();
         }
 
         public override void Dispose() {
-            Exec(Connection, "use master");
-            foreach(var name in _dropList)
-                Exec(Connection, "drop database " + name);
-
             Connection.Close();
         }
 
         public override void SetUpDatabase() {
-            var name = GenerateTempDbName();
-            _dropList.Add(name);
+            _dbName = GenerateTempDbName();
 
-            Exec(Connection, "create database " + name);
-            Exec(Connection, "use " + name);
+            Exec(Connection, @$"create database {_dbName}
+                    on (name={_dbName},     filename='{MdfPath}')
+                log on (name={_dbName}_log, filename='{LdfPath}')");
+
+            Exec(Connection, "use " + _dbName);
         }
 
         public override void TearDownDatabase() {
+            Exec(Connection, "use master");
+            Exec(Connection, "sp_detach_db " + _dbName);
+            File.Delete(MdfPath);
+            File.Delete(LdfPath);
         }
+
+        string MdfPath => GetDbFilePath(".mdf");
+        string LdfPath => GetDbFilePath(".ldf");
+
+        string GetDbFilePath(string ext) => Path.Combine(Path.GetTempPath(), _dbName + ext);
     }
 
 }
